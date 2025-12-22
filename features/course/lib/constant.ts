a121315d1e030be0411b43a6178545d6bc0625aa
chayandasgupta <chayandas285178@gmail.com>
2025-12-22 12:53:48 +0600

    feat(tab-status): add tab status constant to handle dynamic course tabs

--- features/course/lib/constant.ts
+++ features/course/lib/constant.ts
@@ -1,11 +1,18 @@
-export const COURSE_FORM_TABS = [
-  { id: "basic", label: "Basic", showInEdit: false },
-  { id: "info", label: "Info", showInEdit: false },
-  { id: "pricing", label: "Pricing", showInEdit: false },
-  { id: "media", label: "Media", showInEdit: false },
-  { id: "seo", label: "SEO", showInEdit: false },
-  { id: "curriculum", label: "Curriculum", showInEdit: true },
-  { id: "finish", label: "Finish & Submit" },
+type CourseTabStatus = "active" | "inactive";
+
+export const COURSE_FORM_TABS: {
+  id: string;
+  label: string;
+  showInEdit?: boolean;
+  status: CourseTabStatus;
+}[] = [
+  { id: "basic", label: "Basic", status: "active" },
+  { id: "info", label: "Info", status: "active" },
+  { id: "pricing", label: "Pricing", status: "active" },
+  { id: "media", label: "Media", status: "active" },
+  { id: "seo", label: "SEO", status: "inactive" }, // hidden
+  { id: "curriculum", label: "Curriculum", showInEdit: true, status: "active" },
+  { id: "finish", label: "Finish & Submit", status: "active" },
 ];
 
 export const COURSE_METRIC_TITLE = {
